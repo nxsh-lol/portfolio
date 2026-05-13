43b6f8e670d00ef9ba19292bce637d17b5c67ca9
Nxsh <67327530+nxsh-lol@users.noreply.github.com>
2026-05-13 13:30:18 -0500

    Update en.ts

--- src/i18n/languages/en.ts
+++ src/i18n/languages/en.ts
@@ -5,7 +5,7 @@ export const en: Translation = {
 	[Key.home]: "Home",
 	[Key.about]: "About",
 	[Key.archive]: "Archive",
-	[Key.adventures]: "Adventures",
+	[Key.adventures]: "Logs",
 	[Key.search]: "Search",
 
 	[Key.tags]: "Tags",
